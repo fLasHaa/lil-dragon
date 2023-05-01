3de0ecd0cb28c4379c4ad70653a60060043cb009
fLasHaa <flavio.chainho@gmail.com>
2023-05-01 19:25:10 +0100

    Enemy Projectile
    Fixed enemy projectiles disappearing mid air as soon as another one hit the player

--- New Game/Assets/Scripts/Traps/EnemyProjectile.cs
+++ New Game/Assets/Scripts/Traps/EnemyProjectile.cs
@@ -8,23 +8,31 @@ public class EnemyProjectile : EnemyDamage //Will damage the player every time t
     [SerializeField] private float resetTime;
 
     private float lifetime;
-
     private Animator anim;
+    private BoxCollider2D coll;
+
+    private bool hit;
+    
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>(); 
     }
 
     public void ActivateProjectile()
     {
+        hit = false;
+
         lifetime = 0;
         gameObject.SetActive(true);
-
+        coll.enabled = true;
     }
 
     private void Update()
     {
+        if (hit) return;
+
         float movementSpeed = speed * Time.deltaTime;
         transform.Translate(movementSpeed, 0, 0);
 
@@ -37,8 +45,9 @@ public class EnemyProjectile : EnemyDamage //Will damage the player every time t
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        hit = true;
         base.OnTriggerEnter2D(collision); //Execute the logic from parent script first 
-        gameObject.SetActive(false); //When this object hits any object -> deactivate arrow
+        coll.enabled = false;
 
         if (anim != null)
             anim.SetTrigger("explode");//When the object is a fireball explode it
